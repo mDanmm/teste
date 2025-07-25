1a3e8ab2429484776ebb3ad89268302112cc8bd0
mDanmm <danielmonnnteiro@gmail.com>
2025-07-24 21:14:42 -0300

    Add files via upload

---
+++ Aula 02/scrip 2.js
@@ -0,0 +1,84 @@
+/*alert('Bom dia')
+alert('Meu nome é Daniel')
+alert('Minha iade é 23 anos')
+alert ('Estou no 2 modulo de desenvolvimento')
+alert ('Quero ser programador')
+alert ('Vamo q Vamo')
+alert('O mundo é nosso')
+
+console.log('Olá')
+console.log('Meu nome é Daniel')
+console.log('Tenho 23 anos ')
+
+
+let nome = prompt ('digite seu nome')
+alert(nome)*/
+
+/*let numeroSoma1 = Number (prompt ('Digite o primeiro numero para Somar'))
+let numeroSoma2 = Number (prompt('Digite outro numero para Somar'))
+let resultado = numeroSoma1 + numeroSoma2
+alert(`O resultado da divisao dos valores ${numeroSoma1}+${numeroSoma2}=${resultado}`);
+
+
+/*let numeroSubtracao3 = Number (prompt ('Digite o primeiro numero para Subtrair'))
+let numerosubtracao4 = Number (prompt('Digite outro numero para Subtrair'))
+let resultado = numeroSubtracao3 - numeroSubtracao4
+alert(`O resultado da divisao dos valores ${numeroSubtracao3}-${numeroSubtracao4}=${resultado}`);
+
+
+
+let numeroMultiplicacao5 = Number (prompt ('Digite o primeiro numero para Multiplicar'))
+let numeroMultiplicacao6 = Number (prompt('Digite outro numero para Multiplicar'))
+let resultado = numeroMultiplicacao5 * numeroMultiplicacao6
+alert(`O resultado da divisao dos valores ${numeroMultiplicacao5}*${numeroMultiplicacao6}=${resultado}`);
+
+
+let nome = 'Daniel'
+let numeroDivisao7 = Number (prompt ('Digite o primeiro numero para Dividir'))
+let numeroDivisao8 = Number (prompt('Digite outro numero para Dividir'))
+let resultado = numeroDivisao7 / numeroDivisao8
+alert(`O resultado da divisao dos valores ${numeroDivisao7}/${numeroDivisao8}=${resultado}`);*/
+
+
+/*let nome = 'area do quadrado'
+let numeroLado1 = Number (prompt ('Digite o primeiro Lado'))
+let numeroLado2 = Number (prompt ('Digite o segunda Lado'))
+let resultado = area = numeroLado1 * numeroLado2
+alert(`O resultado da area dos valores ${numeroLado1}*${numeroLado2}=${resultado}`);*/
+
+
+/*let nome = 'area do retangulo'
+let numeroBase1 = Number (prompt ('Digite o Numero da Base'))
+let numeroAltura2 = Number (prompt ('Digite o Numero da Altura'))
+let resultado = area = numeroBase1 * numeroAltura2
+alert(`O resultado da area do retangulo é ${numeroBase1}*${numeroAltura2}=${resultado}`);*/
+
+/*let nome = 'area do triangulo'
+let numeroBase1 = Number (prompt ('Digite o Numero da Base'))
+let numeroAltura2 = Number (prompt ('Digite o Numero da Altura'))
+let numeroDivisao2 = 2
+let resultado = area = numeroBase1 * numeroAltura2 / numeroDivisao2
+alert(`O resultado da area do triangulo é ${numeroBase1}*${numeroAltura2} /2 =${resultado}`);*/
+
+
+/*let nome = 'area do losangulo'
+let numeroDiametroMenor1 = Number (prompt ('Digite o Numero do Diametro Maior'))
+let numeroDiametroMenor2 = Number (prompt ('Digite o Numero do Diametro Menor'))
+let numeroDivisao2 = 2
+let resultado = area = numeroDiametroMenor1 * numeroDiametroMenor2 / numeroDivisao2
+alert(`O resultado da area do Losangulo é ${numeroDiametroMenor1}*${numeroDiametroMenor2} /2 =${resultado}`);*/
+
+/*let nome = 'area do Trapezio'
+let numeroBase2 = Number (prompt ('Digite o Numero do Base Maior'))
+let numeroBase3 = Number (prompt ('Digite o Numero do Base Menor'))
+let numeroAltura001 = Number (prompt ('Digite o Numero da Altura'))
+let numeroDivisao2 = 2
+let resultado = area = numeroBase2 + numeroBase3 * numeroAltura001  / numeroDivisao2
+alert(`O resultado da area do Trapezio é ${numeroBase2}+${numeroBase3}*${numeroAltura001} /2 =${resultado}`);*/
+
+
+let nome = 'area do Circulo'
+let numeroTT = 3.14
+let numeroquadrado1 = Number (prompt ('Digite o Numero do Raio ao Quadrado'))
+let resultado = area = numeroTT * numeroquadrado1 * numeroquadrado1
+alert(`O resultado da area do Circulo é ${numeroTT}*${numeroquadrado1}*${numeroquadrado1} =${resultado}`);
