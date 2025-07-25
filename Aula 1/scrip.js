2a0c6f3534eaeef2a9180328e0adb2537bf73023
mDanmm <danielmonnnteiro@gmail.com>
2025-07-24 21:12:58 -0300

    Add files via upload

---
+++ Aula 1/scrip.js
@@ -0,0 +1,27 @@
+//const idade = 23
+//let nome = "Daniel"
+
+//alert("MEU NOME É " + nome)
+//alert("Minha idade é " + idade)
+
+let numero1 = 10
+let numero2 = 5
+let resultado_Soma = numero1 + numero2
+let resultado_subtracao = numero1 - numero2
+let resultado_divisao = numero1 / numero2
+let resultado_multiplicacao = numero1 * numero2
+alert("Resultado da Soma dos valores:" + numero1+ "+" + numero2 + "=" + resultado_Soma)
+alert("Resultado da Soma dos valores:" + numero1+ "-" + numero2 + "=" + resultado_subtracao)
+alert("Resultado da Soma dos valores:" + numero1+ "/" + numero2 + "=" + resultado_divisao)
+alert("Resultado da Soma dos valores:" + numero1+ "*" + numero2 + "=" + resultado_multiplicacao)
+
+
+if (funcao =="+"){ 
+    resultado_Soma = numero1 + numero2
+
+}
+    
+if (funcao =="-"){ 
+    resultado_Soma = numero1 + numero2
+
+}
